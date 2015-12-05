cea723c1ffd0e05b873da55fbfb821818b954158
Sean Clay <sclay@bobclayschili.com>
2015-12-05 00:06:14 -0800

    First commit

---
+++ Magic8Ball.py
@@ -0,0 +1,20 @@
+# Magic 8 Ball - Astro Pi
+
+import random
+import time
+
+print("Ask a question")
+time.sleep(3)
+
+replies = ['Signs point to yes',
+           'Without a doubt',
+           'You may rely on it',
+           'Do not count on it',
+           'Looking good',
+           'Cannot predict now',
+           'It is decidedly so',
+           'Outlook not so good'
+           ]
+
+print(random.choice(replies))
+
